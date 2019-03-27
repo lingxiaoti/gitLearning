d05f6daab6c04b2c8c85cde334a86cf2e0c0f6ef
guoyago <guoyang.csu@outlook.com>
2019-03-27 15:34:04 +0800

    新增

--- src/main/java/gitTest/Guo.java
+++ src/main/java/gitTest/Guo.java
@@ -5,4 +5,28 @@ public class Guo {
     private String username;
     private Long number;
     private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Long getNumber() {
+        return number;
+    }
+
+    public void setNumber(Long number) {
+        this.number = number;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
