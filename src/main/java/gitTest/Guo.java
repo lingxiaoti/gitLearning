9a96f9c81fecc91343c9ee20eb83f59df821e05b
guoyago <guoyang.csu@outlook.com>
2019-03-27 15:35:30 +0800

    新增

--- src/main/java/gitTest/Guo.java
+++ src/main/java/gitTest/Guo.java
@@ -5,4 +5,13 @@ public class Guo {
     private String username;
     private Long number;
     private String password;
+
+    @Override
+    public String toString() {
+        return "Guo{" +
+                "username='" + username + '\'' +
+                ", number=" + number +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
